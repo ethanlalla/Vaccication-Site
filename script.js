2b66be63504f1137afa103f5909faa7d59f9515f
EthanolCB <ethanlalla300@gmail.com>
2021-10-31 13:55:15 -0400

    added js script

---
+++ script.js
@@ -0,0 +1,11 @@
+var vaccinated = true;
+
+var name= ' Chrsitne ';
+
+var message = 'Hi' + name + 'Vaccine Approved, Welcome to Massy Stores!';
+
+var element = document.getElementById('AYV');
+
+element.textContent = message;
+
+console.log(message);
