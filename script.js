7d873a7c634e48b3c97e92d6e4db9b9f77a98c08
EthanolCB <ethanlalla300@gmail.com>
2021-11-06 16:48:59 -0400

    bug fixing

--- script.js
+++ script.js
@@ -1,42 +1,7 @@
-var vaccineCard = new Object();
-var vaccineCard = {
-  name: 'Chris Oneal',
-  vaccinationLocation: 'Napa',
-  type:'Astra Zenica',
-  secondDose: new Date('August 10, 2021 01:00:00'),
-  fullyVacced: true,
-  checkDifference: function(){
-    var today = new Date();
-    var difference = today.getTime() - vaccineCard.secondDose.gettime();
-    difference = Math.floor(difference/ 90000000);
-    return difference;
-  }
-};
-
-var date = vaccineCard.checkDifference();
-
-var daysUntil = 14 - date;
-
-if (vaccineCard.fullyVaccinated == true) {
-  if (date >= 14) {
-    var message = document.getElementById("message");
-    message.textContent = "Vaccination Card Verified!";
-  }
-  else{
-    var message = document.getElementById("message");
-    message.textContent = "There are" + daysUntil + "days until may be allowed to enter";
-    }
-  else{
-    var message = document.getElementById("message");
-    message.textContent = "Vaccination Card was unable to be Verified. Unfortunatley we cannot allow you access to our buisness";
-  }
-}
-
-
 
 var vaccinated = false;
 
-var name=  'Chros Oneal';
+var name=  'Chris Oneal';
 
 var message = 'Hi' + name + 'Vaccine Approved, Welcome to Massy Stores!';
 
@@ -45,3 +10,50 @@ var elName = document.getElementById('AYV');
 elName.textContent = message;
 
 console.log(message);
+
+
+
+var vaccinated = false;
+
+
+var name = 'Chris Oneal';
+
+var message = 'Hi ' + name + ' ,Vaccine Approved, Welcome to MovieTowne!';
+
+var elName = document.getElementById('AYV');
+
+elName.textContent = message;
+
+console.log(message);
+
+
+
+var vaccineCard = {
+  name: 'Chris Oneal',
+  vaccinationLocation: 'Napa',
+  type: 'Astra Zenica',
+  secondDose: new Date('Nov 2, 2021 01:00:00'),
+  fullyVacced: true,
+  getDifference:function(){
+    var today = new Date()
+    var difference = today.getTime() - vaccineCard.secondDose.getTime();
+    difference = Math.floor(difference/84600000);
+    return difference;
+  }
+};
+
+var date = vaccineCard.getDifference();
+var daysUntil = 14 - date;
+
+
+if (vaccineCard.fullyVacced == true) {
+ if (date >= 14) {
+ var message = document.getElementById("message");
+ message.textContent= "Your Vaccination has be verified enjoy your Movie!";
+ } else {
+   var message = document.getElementById("message");
+    message.textContent= "There are " + daysUntil + " until you will be fully vaccinated. Please try again then";
+ }
+ } else { var message = document.getElementById("message");
+    message.textContent= "Unfortunatley you are not vaccinated and cannot be allowed entry."
+}
